ca68be7bb3a95d21ac85f69c2dba053b557d817f
Zathiald <A01705403@tec.mx>
2024-03-21 23:12:57 -0600

    Agregar comentarios y modificar acorde a flake8

--- tictactoe.py
+++ tictactoe.py
@@ -8,70 +8,73 @@ Exercises
 4. How could you create a computer player?
 """
 
-from turtle import *
 
+# Importamos el módulo turtle
+import turtle
 from freegames import line
 
 # Inicializa el tablero como una lista de listas
 board = [['' for _ in range(3)] for _ in range(3)]
 
+
+# Dibujamos la cuadrícula del juego tic-tac-toe.
 def grid():
-    """Draw tic-tac-toe grid."""
     line(-67, 200, -67, -200)
     line(67, 200, 67, -200)
     line(-200, -67, 200, -67)
     line(-200, 67, 200, 67)
 
 
+# Dibujamos el jugador X
 def drawx(x, y):
-    """Draw X player."""
-    color('red')
-    width(2.5)
+    turtle.color('red')
+    turtle.width(2.5)
     line(x + 35, y + 33, x + 100, y + 100)
     line(x + 35, y + 100, x + 100, y + 33)
 
 
+# Dibujamos el jugador O
 def drawo(x, y):
-    """Draw O player."""
-    color('blue')
-    width(3)
-    up()
-    goto(x + 67, y + 33)
-    down()
-    circle(33)
+    turtle.color('blue')
+    turtle.width(3)
+    turtle.up()
+    turtle.goto(x + 67, y + 33)
+    turtle.down()
+    turtle.circle(33)
 
 
+# Redondeamos el valor hacia abajo a la cuadrícula con tamaño de cuadrado 133.
 def floor(value):
-    """Round value down to grid with square size 133."""
     return ((value + 200) // 133) * 133 - 200
 
 
+# Inicializamos el estado del juego
 state = {'player': 0}
 players = [drawx, drawo]
 
 
+# Dibujamos X o O en el cuadrado seleccionado.
 def tap(x, y):
-    """Draw X or O in tapped square."""
     x = floor(x)
     y = floor(y)
 
-    # Convierte las coordenadas a índices de la lista
+    # Convertimos las coordenadas a índices de la lista
     i, j = int((x + 200) // 133), int((y + 200) // 133)
-    
-    # Verifica si la casilla está vacía
+    # Verificamos si la casilla está vacía
     if board[i][j] == '':
         player = state['player']
         draw = players[player]
         draw(x, y)
         board[i][j] = 'X' if player == 0 else 'O'
-        update()
+        turtle.update()
         state['player'] = not player
 
 
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
+# Configuramos la ventana del juego
+turtle.setup(420, 420, 370, 0)
+turtle.hideturtle()
+turtle.tracer(False)
 grid()
-update()
-onscreenclick(tap)
-done()
+turtle.update()
+turtle.onscreenclick(tap)
+turtle.done()
